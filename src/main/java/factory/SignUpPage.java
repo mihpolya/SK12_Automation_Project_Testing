0e87616478c0cdb5b85d208e5d17a36747387f75
mihpolya <157997159+mihpolya@users.noreply.github.com>
2024-04-17 07:20:13 +0300

    started the registration test

---
+++ src/main/java/factory/SignUpPage.java
@@ -0,0 +1,28 @@
+package factory;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignUpPage {
+    public static final String PAGE_URL = "http://training.skillo-bg.com:4300/users/register";
+    private final WebDriver webDriver;
+
+    public SignUpPage(WebDriver driver){
+        this.webDriver = driver;
+        PageFactory.initElements(webDriver(), this);
+    }
+    public WebDriver webDriver()
+    {
+        return this.webDriver;
+    }
+
+
+    private String generateRandomEmail(int minLengthInclusive, int maxLengthInclusive) {
+        return generateRandomAlphabeticString(minLengthInclusive, maxLengthInclusive) + "@gmail.com";
+    }
+
+    private String generateRandomAlphabeticString(int minLengthInclusive, int maxLengthInclusive) {
+        return RandomStringUtils.randomAlphanumeric(minLengthInclusive, maxLengthInclusive);
+    }
+}
